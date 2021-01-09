061bdb5b9e7d988ba9df586da9e5cb4d28c9e7f2
nicolasM <n.martini@live.fr>
2021-01-09 10:05:36 +0100

    mon commit git

--- index.php
+++ index.php
@@ -6,6 +6,13 @@
     <title>Document</title>
 </head>
 <body>
+    <section>
     
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas voluptatibus soluta neque? Id itaque quas voluptate porro recusandae. Mollitia laudantium ab eaque nobis ipsam similique sed, cupiditate cum tenetur voluptatibus.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, animi. Quisquam facere sapiente animi pariatur reprehenderit deserunt nulla cupiditate rerum nam dicta delectus, similique id repellat eos. Magni, quae? Inventore!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur quas repellendus ad nihil quae saepe iusto autem quaerat, non voluptatibus et perspiciatis iure dolorum cum atque rerum quod soluta unde?
+
+    
+    </section>
 </body>
 </html>
